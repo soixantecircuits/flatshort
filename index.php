3bef3590c2aa6b842cefb58f9a10ee157987c11d
gabrielstuff <gabriel@soixantecircuits.fr>
2015-09-15 17:39:00 +0200

    adding readme

--- index.php
+++ index.php
@@ -4,23 +4,21 @@ use Crisu83\ShortId\ShortId;
 require(__DIR__ . '/./vendor/autoload.php');
 $shortid = ShortId::create();
 
-$redirect_URI = "http://webtips.krajee.com/?id=";
-if ( isset ( $_GET['id'] ) ) {
-    $id = $_GET['id'];
+if ( isset ( $_GET['uri'] ) ) {
+    $uri = $_GET['uri'];
 } else {
   header('HTTP/1.1 503 OK');
   header('Content-Type: application/json');
-  echo json_encode(array('error'=> "No id specify"));
+  echo json_encode(array('error'=> "No uri specify"));
   die();
 }
-
 if(isset ( $_GET['shortid'] ) ){
   $short_id = $_GET['shortid'];
 } else {
   $short_id = $shortid->generate();
 }
 $myfile = fopen($short_id.".php", "w") or die("Unable to open file!");
-$txt = '<?php header("Location: ' .$redirect_URI.$id.'");?>';
+$txt = '<?php header("Location: ' .$uri.'");?>';
 fwrite($myfile, $txt);
 fclose($myfile);
 header('HTTP/1.1 200 OK');
